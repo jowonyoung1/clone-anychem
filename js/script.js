9a83f6cd3ef71145333e89f9c8c20478784edd10
jowonyoung1 <jwy7254@naver.com>
2024-09-18 21:14:06 +0900

    저작권 팝업창 추가 및 리드미 수정

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 window.addEventListener("load", function () {
   AOS.init();
 });
+
+// 저작권 안내 팝업창
+window.addEventListener("load", function () {
+  const closeBt = document.querySelector(".popup-close");
+  const popup = document.querySelector(".popup");
+  closeBt.addEventListener("click", function () {
+    popup.style.display = "none";
+  });
+});
